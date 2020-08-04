3ae5be4ea9eb2539d82e0726eeb175ae520dba1c
amandasoaares <amanda14_sc@hotmail.com>
2020-08-04 20:33:58 -0300

    Create test ECS with EC2

---
+++ examples/ecs_ec2/main.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "cicd"
+}
+
+module "vpc" {
+  source = "github.com/youse-seguradora/terraform-aws-vpc"
+
+  name = var.vpc_name
+
+  cidr = "192.168.253.0/24"
+
+  azs                    = ["us-east-2a"]
+  compute_public_subnets = ["192.168.253.0/24"]
+}
+
+
+module "ecs_cluster" {
+  source = "github.com/youse-seguradora/terraform-aws-ecs-cluster"
+
+  ecs_cluster_name = var.ecs_cluster_name
+}
+
+module "ecs_task_def" {
+  source = "github.com/youse-seguradora/terraform-aws-ecs-task-definition"
+
+  name                     = var.app_name
+  container_definitions    = file("task.json")
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
+}
+
+module "ecs_service" {
+  source = "../.."
+
+  service_name          = var.app_name
+  service_launch_type   = "EC2"
+  service_desired_count = 1
+
+  ecs_cluster_id          = module.ecs_cluster.cluster_id
+  ecs_task_definition_arn = module.ecs_task_def.arn
+
+  awsvpc_service_map = [{
+    security_groups = [module.vpc.default_security_group_id]
+    subnets         = module.vpc.public_subnets
+  }]
+
+
+  # Setting app autoscaling target
+  resource_id  = "service/${var.ecs_cluster_name}/${var.app_name}"
+  max_capacity = 1
+
+  # Settings app autoscaling policy
+  name_policy = "${var.app_name}-CPUUtilization"
+
+  placement_strategy = [{
+    type  = "binpack"
+    field = "memory"
+  }]
+}
+
+variable "vpc_name" {
+  default = "test_ecs_service"
+}
+variable "ecs_cluster_name" {
+  default = "test_ecs_service"
+}
+variable "app_name" {}
+
+
+output "ecs_cluster_id" {
+  value = module.ecs_cluster.cluster_id
+}
+output "ecs_task_def_arm" {
+  value = module.ecs_task_def.arn
+}
+output "vpc_security_group_id" {
+  value = module.vpc.default_security_group_id
+}
+output "vpc_public_subnets" {
+  value = module.vpc.public_subnets
+}
+output "desired_count" {
+  value = module.ecs_service.desired_count
+}
+
+output "app_name" {
+  value = module.ecs_service.name
+}
